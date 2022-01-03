8e8dc5ce06aa641265bc26b0e198f6b970c17947
SujinRoh <suuz.hha@gmail.com>
2022-01-03 18:17:43 +0900

    강의 시청 내용 코딩

---
+++ 강의 시청 내용/main.js
@@ -0,0 +1,166 @@
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+var qs = require('querystring');
+
+function fnTemplateHTML(title, list, body, control) {
+  return `
+ <!doctype html>
+ <html>
+ <head>
+ <title>WEB1 - ${title}</title>
+ <meta charset="utf-8">
+ </head>
+ <body>
+ <h1><a href="/">WEB</a></h1>
+ ${list}
+ ${control}
+ ${body}
+ </body>
+ </html>
+ `;
+}
+
+function fnTemplateList(fileList) {
+  var list = "<ul>";
+  for (var i = 0; i < fileList.length; i++) {
+    list += `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+  }
+  list += "</ul>";
+  return list;
+}
+
+var app = http.createServer(function (request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
+  if (pathname === "/") {
+    if (queryData.id === undefined) {
+      fs.readdir("./data", function (error, fileList) {
+        var title = "welcome";
+        var description = "hello, node.js";
+        var list = fnTemplateList(fileList);
+        var template = fnTemplateHTML( title, list,
+          `<h2>${title}</h2>${description}`,
+          `<a href="/create">create</a>`);
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else {
+      fs.readdir("./data", function (error, fileList) {
+        fs.readFile( `data/${queryData.id}`, "utf8", function (err, description) {
+          var title = queryData.id;
+          var list = fnTemplateList(fileList);
+          var template = fnTemplateHTML(title, list,
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a> 
+             <a href="/update?id=${title}">update</a>
+             <form action="delete_process" method="post" onsubmit="">
+              <input type="hidden" name="id" value="${title}">
+              <input type="submit" value="delete">
+             </form>`);
+          response.writeHead(200);
+          response.end(template);
+          }
+        );
+      });
+    }
+  } else if(pathname === '/create') {
+    fs.readdir("./data", function (error, fileList) {
+      var title = "WEB - create";
+      var list = fnTemplateList(fileList);
+      var template = fnTemplateHTML( title, list,
+        `
+        <form action="/create_process" method="post"> 
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p>
+        <textarea name="description" placeholder="description"></textarea>
+        </p>
+        <p>
+        <input type="submit">
+        </p>
+        </form>
+        `,
+        '');
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else if(pathname === '/create_process') {
+    var body = '';
+    request.on('data', function(data){
+      body = body + data;
+    });
+    request.on('end', function(){
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+
+      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+        if (err) throw err;
+        response.writeHead(302,{Location: `/?id=${title}`});
+        response.end();
+      });
+    });
+  } else if(pathname === '/update') {
+    fs.readdir("./data", function (error, fileList) {
+      fs.readFile( `data/${queryData.id}`, "utf8", function (err, description) {
+        var title = queryData.id;
+        var list = fnTemplateList(fileList);
+        var template = fnTemplateHTML(title, list,
+          `
+          <form action="/update_process" method="post"> 
+          <input type="hidden" name="id" value="${title}">
+          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+          <p>
+          <textarea name="description" placeholder="description">
+          ${description}
+          </textarea>
+          </p>
+          <p>
+          <input type="submit">
+          </p>
+          </form>
+          `,
+          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+        response.writeHead(200);
+        response.end(template);
+        }
+      );
+    });
+  } else if(pathname === '/update_process') {
+    var body = '';
+    request.on('data', function(data){
+      body = body + data;
+    });
+    request.on('end', function(){
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var description = post.description;
+      fs.rename(`data/${id}`, `data/${title}`, function(error){
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+          if (err) throw err;
+          response.writeHead(302,{Location: `/?id=${title}`});
+          response.end();
+        });
+      });
+    });
+  } else if(pathname === '/delete_process') {
+    var body = '';
+    request.on('data', function(data){
+      body = body + data;
+    });
+    request.on('end', function(){
+      var post = qs.parse(body);
+      var id = post.id;
+      fs.unlink(`data/${id}`, function(err){
+        response.writeHead(302,{Location: '/'});
+        response.end();
+      });
+    });
+  } else {
+    response.writeHead(404);
+    response.end("Not found");
+  }
+});
+app.listen(3000);
---
+++ 강의 시청 내용/syntax/callback.js
@@ -0,0 +1,15 @@
+// function a() {
+
+//     console.log('a');
+// }
+
+var a = function() {
+
+    console.log('a');
+}
+
+function slowfunc(callback) {
+    callback();
+}
+
+slowfunc(a); 
